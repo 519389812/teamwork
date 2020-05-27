e638365b5d179cda6d692d8f77c81e1a4e437667
519389812 <519389812>
2020-05-27 15:43:07 +0800

    27may20 08

--- task.py
+++ task.py
@@ -1,16 +1,16 @@
 from django.utils import timezone
-from announcement import models
+from announcement.models import Announcement, image_path
 import os
 from teamwork import settings
 
 
 def clean_expired_data():
-    data = models.Announcement.objects.filter(deadline__lte=timezone.now(), active=True)
+    data = Announcement.objects.filter(deadline__lte=timezone.now(), active=True)
     if len(data) > 0:
         data_id = list(set(data.values_list("id", flat=True)))
         data.update(content="Expired", active=False)
         data_id = [str(i) for i in data_id]
-        dir = os.path.join(settings.MEDIA_ROOT, models.image_path)
+        dir = os.path.join(settings.MEDIA_ROOT, image_path)
         for file in os.listdir(dir):
             if file.startswith(tuple(data_id)):
                 os.remove(os.path.join(dir, file))
